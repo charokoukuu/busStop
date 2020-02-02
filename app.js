944d84c355596c6b386edab8070fd016fbfb1ccf
Hinata Saito <charokoukuu@gmail.com>
2020-02-02 20:19:56 +0900

    _

--- app.js
+++ app.js
@@ -3,18 +3,20 @@ var express = require('express'),
 var add = "0";
 var add2 = "0";
 var sumiregaoka = require("./json/timeTableSumiregaoka.json");
+var takarazuka = require("./json/timeTableTakarazuka.json");
 var debug = 0;
+var text = "";
 require('date-utils');
 app.set('views', __dirname + "/views");
 app.set('view engine', 'ejs');
-
+const lineNotify = require('line-notify-nodejs')('ubnCYGu4bTMEDw2QCkO0U6pNMTVbj305HFDxc06lYdL'); // 先ほどコピーしたトークン
 app.use(express.static(__dirname + '/public'));
 // app.use((req, res, next) => {
 //   res.send("Custom!");
 //   next();
 // });
 
-app.get('/', (req, res) => {
+app.get('/sumi', (req, res) => {
   var dt = new Date();
   var formatted = dt.toFormat("H");
   var formatted2 = dt.toFormat("MI");
@@ -22,18 +24,51 @@ app.get('/', (req, res) => {
   add = debug;
   add2 = debug + 1;
   if (formatted2 > 45) {
-    res.send(formatted + "時代：" + "[" + sumiregaoka.weekdays[add].filter(value => {
+    text = formatted + "時代：" + "[" + sumiregaoka.weekdays[add].filter(value => {
       return value > formatted2;
-    }) + "]" + "\n" +
-      add2.toString() + "時代：" + "[" + sumiregaoka.weekdays[add2].filter(value => {
+    }) + "]" + "\n" + "[" +
+      add2.toString() + "時代：" + sumiregaoka.weekdays[add2].filter(value => {
         return value < 15;
-      }) + "]"
+      }) + "]";
 
-    );
+
   } else {
-    res.send(formatted + "時代：" + "[" + sumiregaoka.weekdays[add].filter(value => {
+    text = formatted + "時代：" + "[" + sumiregaoka.weekdays[add].filter(value => {
       return value > formatted2;
-    }) + "]");
+    });
+    lineNotify.notify({
+      message: text,
+    }) + "]";
+  }
+  // if (formatted2 > 45) {
+
+  // }
+  console.log(add);
+  console.log(add2);
+});
+app.get('/taka', (req, res) => {
+  var dt = new Date();
+  var formatted = dt.toFormat("H");
+  var formatted2 = dt.toFormat("MI");
+  debug = Number(formatted);
+  add = debug;
+  add2 = debug + 1;
+  if (formatted2 > 45) {
+    text = formatted + "時代：" + "[" + takarazuka.weekdays[add].filter(value => {
+      return value > formatted2;
+    }) + "]" + "\n" + "[" +
+      add2.toString() + "時代：" + takarazuka.weekdays[add2].filter(value => {
+        return value < 15;
+      }) + "]";
+
+
+  } else {
+    text = formatted + "時代：" + "[" + takarazuka.weekdays[add].filter(value => {
+      return value > formatted2;
+    });
+    lineNotify.notify({
+      message: text,
+    }) + "]";
   }
   // if (formatted2 > 45) {
 
